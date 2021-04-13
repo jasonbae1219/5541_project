f9b49839b5487b609f61a89c3a2e5c83042e526d
Jie.Dai <jdai.dev@gmail.com>
2021-04-13 14:39:20 -0400

    Several major refactoring 1. implemented change log 2. implemented customized Action classes 3. nested actions

--- src/GUI.java
+++ src/GUI.java
@@ -1,137 +1,212 @@
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.event.UndoableEditEvent;
 import javax.swing.event.UndoableEditListener;
-import javax.swing.undo.UndoManager;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.text.*;
+import java.awt.*;
+import java.util.HashMap;
 
-public class GUI implements ActionListener {
-    JFrame window;
-    JTextArea textArea;
-    JScrollPane scrollPane;
+public class GUI extends JFrame {
+    JTextPane textPane;
+    AbstractDocument doc;
+    JTextArea changeLog;
+    JScrollPane scrollPane, scrollPaneForLog;
+    JSplitPane splitPane;
+    String newline = "\n";
+    HashMap<Object, Action> actions;
 
     // top menu bar
     JMenuBar menuBar;
-    JMenu menuFile, menuEdit;
 
     // file menu
-    JMenuItem miNew, miOpen, miSave, miSaveAs, miExit;
-    // edit menu
-    JMenuItem miUndo, miRedo;
+    FileManager file = new FileManager(this);
+    protected FileManager.NewAction newAction;
+    protected FileManager.OpenAction openAction;
+    protected FileManager.SaveAction saveAction;
+    protected FileManager.SaveAsAction saveAsAction;
+    protected FileManager.ExitAction exitAction;
 
-    FuncFile file = new FuncFile(this);
-    FuncEdit edit = new FuncEdit(this);
-
-    UndoManager um = new UndoManager();
+    // undo helpers
+    protected SmartUndoManager um = new SmartUndoManager(this);
+    protected SmartUndoManager.UndoAction undoAction;
+    protected SmartUndoManager.RedoAction redoAction;
 
     public GUI() {
-        createWindow();
-        createTextArea();
+        // set main window
+        super("Smart Text Editor"); // design: name
+        setSize(800, 600); // design: default size
+        setLayout(new BorderLayout()); // layout: Border Layout
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // design: default closing behavior
+
+        createTextPane();
+        createSidePanel();
+        createSplitPane();
         createMenuBar();
-        createFileMenu();
-        createEditMenu();
-        window.setVisible(true);
+        setVisible(true);
+
+        // start listening for edits
+        addListeners();
     }
 
-    public void createWindow() {
-        window = new JFrame("Smart Text Editor"); // design: name
-        window.setSize(800, 600); // design: default size
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // design: default closing behavior
-    }
+    public void createTextPane() {
+        textPane = new JTextPane();
+        StyledDocument styleDoc = textPane.getStyledDocument();
+        if (styleDoc instanceof AbstractDocument) {
+            doc = (AbstractDocument) styleDoc;
+        } else {
+            System.err.println("Text pane's document isn't an AbstractDocument!");
+            System.exit(-1);
+        }
 
-    public void createTextArea() {
-        textArea = new JTextArea();
-        textArea.getDocument().addUndoableEditListener(
-                new UndoableEditListener() {
-                    @Override
-                    public void undoableEditHappened(UndoableEditEvent e) {
-                        um.addEdit(e.getEdit());
-                    }
-                }
-        );
-        scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane = new JScrollPane(textPane);
         scrollPane.setBorder(BorderFactory.createEmptyBorder()); // remove scroll pane boarder
-        window.add(scrollPane);
+    }
+
+    public void createSidePanel(){
+        changeLog = new JTextArea();
+        changeLog.setEditable(false);
+        scrollPaneForLog = new JScrollPane(changeLog);
+    }
+
+    public void createSplitPane() {
+        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollPane, scrollPaneForLog);
+
+        scrollPane.setMinimumSize(new Dimension(500,600));
+        scrollPaneForLog.setMinimumSize(new Dimension(150, 600));
+
+        splitPane.setOneTouchExpandable(true);
+        splitPane.setDividerLocation(0.75);
+        this.getContentPane().add(splitPane, BorderLayout.CENTER);
     }
 
     public void createMenuBar() {
+        actions = createActionTable(textPane);
+        JMenu fileMenu = createFileMenu();
+        JMenu editMenu = createEditMenu();
+        JMenu styleMenu = createStyleMenu();
         menuBar = new JMenuBar();
-        window.setJMenuBar(menuBar);
-
-        menuFile = new JMenu("File");
-        menuEdit = new JMenu("Edit");
-
-        menuBar.add(menuFile);
-        menuBar.add(menuEdit);
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(styleMenu);
+        this.setJMenuBar(menuBar);
     }
 
-    public void createFileMenu() {
-        miNew = new JMenuItem("New");
-        miNew.addActionListener(this);
-        miNew.setActionCommand("New");
+    protected JMenu createEditMenu() {
+        JMenu menu = new JMenu("Edit");
 
-        miOpen = new JMenuItem("Open");
-        miOpen.addActionListener(this);
-        miOpen.setActionCommand("Open");
+        undoAction = um.new UndoAction();
+        menu.add(undoAction);
 
-        miSave = new JMenuItem("Save");
-        miSave.addActionListener(this);
-        miSave.setActionCommand("Save");
+        redoAction = um.new RedoAction();
+        menu.add(redoAction);
 
-        miSaveAs = new JMenuItem("Save As");
-        miSaveAs.addActionListener(this);
-        miSaveAs.setActionCommand("SaveAs");
+        menu.addSeparator();
 
-        miExit = new JMenuItem("Exit");
-        miExit.addActionListener(this);
-        miExit.setActionCommand("Exit");
+        // default editor kit
+        menu.add(getActionByName(DefaultEditorKit.copyAction));
+        menu.add(getActionByName(DefaultEditorKit.copyAction));
+        menu.add(getActionByName(DefaultEditorKit.pasteAction));
 
-        menuFile.add(miNew);
-        menuFile.add(miOpen);
-        menuFile.add(miSave);
-        menuFile.add(miSaveAs);
-        menuFile.add(miExit);
+        menu.addSeparator();
+
+        menu.add(getActionByName(DefaultEditorKit.selectAllAction));
+        return menu;
     }
 
-    public void createEditMenu() {
-        miUndo = new JMenuItem("Undo");
-        miUndo.addActionListener(this);
-        miUndo.setActionCommand("Undo");
+    protected JMenu createStyleMenu(){
+        JMenu menu = new JMenu("Style");
 
-        miRedo = new JMenuItem("Redo");
-        miRedo.addActionListener(this);
-        miRedo.setActionCommand("Redo");
+        Action action = new StyledEditorKit.BoldAction();
+        action.putValue(Action.NAME, "Bold");
+        menu.add(action);
 
-        menuEdit.add(miUndo);
-        menuEdit.add(miRedo);
+        action = new StyledEditorKit.ItalicAction();
+        action.putValue(Action.NAME, "Italic");
+        menu.add(action);
+
+        action = new StyledEditorKit.UnderlineAction();
+        action.putValue(Action.NAME, "Underline");
+        menu.add(action);
+
+        menu.addSeparator();
+
+        menu.add(new StyledEditorKit.FontSizeAction("12", 12));
+        menu.add(new StyledEditorKit.FontSizeAction("14", 14));
+        menu.add(new StyledEditorKit.FontSizeAction("18", 18));
+
+        menu.addSeparator();
+
+        menu.add(new StyledEditorKit.FontFamilyAction("Serif","Serif"));
+        menu.add(new StyledEditorKit.FontFamilyAction("SansSerif","SansSerif"));
+
+        menu.addSeparator();
+
+        menu.add(new StyledEditorKit.ForegroundAction("Red", Color.red));
+        menu.add(new StyledEditorKit.ForegroundAction("Green", Color.green));
+        menu.add(new StyledEditorKit.ForegroundAction("Blue", Color.blue));
+        menu.add(new StyledEditorKit.ForegroundAction("Black", Color.black));
+
+        return menu;
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String command = e.getActionCommand();
+    private HashMap<Object, Action> createActionTable(JTextComponent textComponent) {
+        HashMap<Object, Action> actions = new HashMap<Object, Action>();
+        Action[] actionsArray = textComponent.getActions();
+        for (Action a : actionsArray) {
+            actions.put(a.getValue(Action.NAME), a);
+        }
+        return actions;
+    }
 
-        switch (command) {
-            case "New":
-                file.newFile();
-                break;
-            case "Open":
-                file.open();
-                break;
-            case "Save":
-                file.save();
-                break;
-            case "SaveAs":
-                file.saveAs();
-                break;
-            case "Exit":
-                file.exit();
-                break;
-            case "Undo":
-                edit.undo();
-                break;
-            case "Redo":
-                edit.redo();
-                break;
+    private Action getActionByName(String name) {
+        return actions.get(name);
+    }
+
+    protected JMenu createFileMenu() {
+        JMenu menu = new JMenu("File");
+
+        newAction = file.new NewAction();
+        openAction = file.new OpenAction();
+        saveAction = file.new SaveAction();
+        saveAsAction = file.new SaveAsAction();
+        exitAction = file.new ExitAction();
+
+        menu.add(newAction);
+        menu.add(openAction);
+        menu.add(saveAction);
+        menu.add(saveAsAction);
+        menu.add(exitAction);
+
+        return menu;
+    }
+
+    // listeners
+    public void addListeners(){
+        doc.addUndoableEditListener(new MyUndoableEditListener());
+        doc.addDocumentListener(new MyDocumentListener());
+    }
+
+    protected class MyUndoableEditListener implements UndoableEditListener {
+        public void undoableEditHappened(UndoableEditEvent e) {
+            um.addEdit(e.getEdit());
+            undoAction.updateUndoState();
+            redoAction.updateRedoState();
+        }
+    }
+
+    protected class MyDocumentListener implements DocumentListener {
+        public void insertUpdate(DocumentEvent e) { displayEditInfo(e);}
+        public void removeUpdate(DocumentEvent e) { displayEditInfo(e);}
+        public void changedUpdate(DocumentEvent e) { displayEditInfo(e);}
+        private void displayEditInfo(DocumentEvent e) {
+            Document document = e.getDocument();
+            int changeLength = e.getLength();
+            changeLog.append(e.getType().toString() + ": " +
+                    changeLength + " character" +
+                    ((changeLength == 1) ? ". " : "s. ") +
+                    " Text length = " + document.getLength() +
+                    "." + newline);
         }
     }
 }
